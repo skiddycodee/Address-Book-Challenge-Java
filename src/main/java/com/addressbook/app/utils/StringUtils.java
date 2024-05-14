1dee4d700f539fd5f33b8dae1363fb6fe61c89bb
eTiggs <edunwo01@student.bbk.ac.uk>
2024-05-14 18:33:14 +0100

    String Utils Tests & regex modified to pass tests

--- src/main/java/com/addressbook/app/utils/StringUtils.java
+++ src/main/java/com/addressbook/app/utils/StringUtils.java
@@ -7,12 +7,12 @@ public abstract class StringUtils {
     }
 
     public static boolean checkIfPhoneNumber(String input) {
-        String phoneNumberRegex = "^\\+?(\\d[\\d-. ]+)?(\\([\\d-. ]+\\))?[\\d-. ]+\\d$";
+        String phoneNumberRegex = "^(?!.*--.*)(?!.*\\.-.*)(?!.*-\\..*)(?!.*\\.\\..*)\\+?\\(?\\d[\\d-. ]+\\)?[-. ]?(\\([\\d-. ]+\\))?[\\d-. ]+\\d$";
         return contactInputQuarantine(input) && input.matches(phoneNumberRegex);
     }
 
     public static boolean checkIfEmail(String input) {
-        String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        String emailRegex = "^(?=[^@]*@[^@]*$)(?!\\.)[A-Za-z0-9+_.-]+(?<!\\.)@((?!\\..*\\.)[A-Za-z0-9][A-Za-z0-9_.-]*[A-Za-z0-9]\\.)+[A-Za-z]{2,6}$";
         return contactInputQuarantine(input) && input.matches(emailRegex);
     }
 
